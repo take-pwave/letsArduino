b498e27f4f3d9ed5d46e76d8c8ff07b1c0c34903
Hiroshi TAKEMOTO <take@pwv.co.jp>
2016-10-15 17:29:42 +0900

    delete test files.

--- setupEnvironment/.build/atmega328/arduino/avr-libc/malloc.d
+++
@@ -1,4 +0,0 @@
-.build/atmega328/arduino/avr-libc/malloc.d .build/atmega328/arduino/avr-libc/malloc.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/malloc.c \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/sectionname.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/stdlib_private.h
--- setupEnvironment/.build/atmega328/arduino/avr-libc/realloc.d
+++
@@ -1,4 +0,0 @@
-.build/atmega328/arduino/avr-libc/realloc.d .build/atmega328/arduino/avr-libc/realloc.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/realloc.c \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/sectionname.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/avr-libc/stdlib_private.h
