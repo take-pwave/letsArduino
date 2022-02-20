96deda48fc90e74c01760a75817cb6d0dba03949
Hiroshi TAKEMOTO <take.pwave@gmail.com>
2022-02-20 18:08:08 +0900

    add 42-QuickChisel.ipynb and QuickChisel

---
+++ QuickChisel/src/wire/README.md
@@ -0,0 +1,7 @@
+# wire
+
+To generate Verilog code, as bellow:
+
+```bash
+sbt run
+```
---
+++ QuickChisel/src/wire/src/main/scala/wire.scala
@@ -0,0 +1,19 @@
+import chisel3._
+import chisel3.stage._
+
+import java.io.PrintWriter
+
+class Wire extends RawModule {
+    val io = IO(new Bundle {
+        val switch = Input(Bool())
+        val led = Output(Bool())
+    })
+
+    io.led := io.switch
+}
+
+object VerilogEmitter extends App {
+    val writer = new PrintWriter("target/wire.v")
+    writer.write(ChiselStage.emitVerilog(new Wire))
+    writer.close()
+}
